fe8d8503364171ed230caf82513453fab708da21
Mykyta Tishkin <87891550+GTeasera@users.noreply.github.com>
2023-03-29 18:18:33 +0300

    spDll

---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿
+
+using SpDll_P05;
+
+using System.Text;
+
+
+
+Console.WriteLine("Hello, World!");
+
+const int MB_RETRYCANCEL = 0x00000005;
+
+const int MB_ICONINFORMATION = 0x00000040;
+
+const int WM_SETTEXT = 0x000C;
+
+const int WM_CLOSE = 0x0010;
+
+
+
+
+
+//var res = WinApiFunctions.MessageBox(IntPtr.Zero, "Some message", "Header", MB_ICONINFORMATION|MB_RETRYCANCEL);
+
+//Console.WriteLine(res);
+
+//var hWnd = WinApiFunctions.FindWindow("WindowsForms10.Window.8.app.0.378734a_r3_ad1", null);
+
+//var hWnd = WinApiFunctions.FindWindow("TTOTAL_CMD", null);
+
+
+
+//WinApiFunctions.SendMessage(hWnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
+
+
+
+//StringBuilder sb = new StringBuilder(300);
+
+//WinApiFunctions.GetWindowText(hWnd,sb,300);
+
+//Console.WriteLine(sb.ToString());
+
+////WinApiFunctions.SetWindowText(hWnd, "Another text");
+
+//WinApiFunctions.SendMessage(hWnd, WM_SETTEXT, IntPtr.Zero, "Sended message");
+
+//WinApiFunctions.GetWindowText(hWnd, sb, 300);
+
+//Console.WriteLine(sb.ToString());
+
+
+
+WinApiFunctions.Beep(2000, 1000);
---
+++ WinApiFunction.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+using System.Collections.Generic;
+
+using System.Linq;
+
+using System.Runtime.InteropServices;
+
+using System.Text;
+
+using System.Threading.Tasks;
+
+
+
+namespace SpDll_P05
+
+{
+
+    internal static class WinApiFunctions
+
+    {
+
+        [DllImport("User32.dll")]
+
+        public static extern int MessageBox(IntPtr hWnd, string msg, string caption, int type);
+
+        [DllImport("User32.dll")]
+
+        public static extern int SetWindowText(IntPtr hWnd, string text);
+
+        [DllImport("User32.dll")]
+
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int capacity);
+
+        [DllImport("User32.dll")]
+
+        public static extern IntPtr FindWindow(string lpClassName, string lpWindowName);
+
+
+
+        [DllImport("User32.dll")]
+
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, string lParam);
+
+        [DllImport("User32.dll")]
+
+        public static extern IntPtr SendMessage(IntPtr hWnd, uint msg, IntPtr wParam, IntPtr lParam);
+
+
+
+        [DllImport("kernel32.dll")]
+
+        public static extern bool Beep(int dwFreq, int dwDuration);
+
+
+
+    }
+
+}
+
